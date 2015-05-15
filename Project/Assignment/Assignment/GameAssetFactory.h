b7d21c073f37fb5e259afc54cb61724fdd079d51
Justin1981 <justin.pettit@hotmail.com.au>
2015-05-15 14:16:51 +0800

    Friday Equalise 1

--- Project/Assignment/Assignment/GameAssetFactory.h
+++ Project/Assignment/Assignment/GameAssetFactory.h
@@ -4,7 +4,11 @@
 
 #include "GameAsset.h"
 
+#include "textureHandler/textureManager.h"
+#include "Texture.h"
+
 #include <string>
+#include <map>
 
 
 class GameAssetFactory
@@ -15,10 +19,21 @@ public:
 
 	~GameAssetFactory();
 
-	GameAsset* Load(std::string filePath);
+	GameAsset* LoadTexture(std::string filePath);
+
+	GameAsset* LoadModel(std::string filePath, std::string texturePath);
+
+	//GameAsset* Load(std::string filePath, int size);
 
 private:
+	//typedef std::map<std::string, GameAsset*> gameAssets;
+	//typedef gameAssets::iterator gaIter;
 
+	//gameAssets m_gameAssets;
+
+
+
+	textureManager m_textureManager;	
 };
 
 #endif
--- Project/Assignment/Assignment/Texture.h
+++ Project/Assignment/Assignment/Texture.h
@@ -2,28 +2,37 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
-#include "Asset.h"
+#include "GameAsset.h"
 
-template <class T>
-struct RGB
-{
-	T r;
-	T g;
-	T b;
-	void set(RGB<T> rgb){r=rgb.r;g=rgb.g;b=rgb.b;}
-    void reset(){r=0;g=0;b=0;}
-};
+#include <string>
 
-class Texture : public Asset
+
+
+class Texture : public GameAsset
 {
 public:
-	virtual bool load(char *filename)=0;
-	virtual unsigned char* getData()=0;
-	virtual int getWidth()=0;
-	virtual int getHeight()=0;
-	virtual void unload()=0;
-	virtual unsigned int getBPP()=0;
-	virtual RGB<unsigned char> getColor(int x,int y)=0;
+
+	
+	Texture();
+
+	Texture(unsigned int textureID);
+
+	//Texture(char* filePath);
+
+	~Texture();
+
+	//void Load(char* filePath);
+
+	void SetTexture(unsigned int texture);
+
+	unsigned int Get();
+
+protected:
+	unsigned int m_texture;
+
+
+private:
+	
 };
 
 #endif
